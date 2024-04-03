8f0dd4a5b676996523386d8310ea99103a3b330f
Reymond Caceres <reymond19890@gmail.com>
2024-04-02 22:57:04 -0400

    responsive

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const menuList = document.getElementById('menuList');
+const menu = document.getElementById('menu');
+
+    menu.addEventListener('click', ()=>{
+        menuList.classList.toggle('-translate-x-full')
+
+    })
+
+const circles = document.querySelectorAll('#circles');
+const cards = document.querySelectorAll('#cards');
+
+circles.forEach( circle =>{
+    circle.addEventListener('click', ()=>{
+        cards.forEach( card =>{
+            card.addEventListener('click', ()=>{
+                card.style.Zindex = 9999;
+            })
+        })
+        })
+    })
